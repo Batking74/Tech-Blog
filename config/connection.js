dfdd139fa0a84d336536576dbb32fc48dfc65e21
Nazir Knuckles <nazirknuckles@gmail.com>
2024-01-11 22:32:36 -0500

    Completed the folder and file structure of the MVC Design. Also, added basic dependencies for development.

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Importing Package
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let database = getDatabase();
+
+
+// Retrieves database connection
+function getDatabase() {
+    if(process.env.JAWSDB_URL) {
+        return new Sequelize(process.env.JAWSDB_URL);
+    }
+    return new Sequelize(process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        })
+}
+
+
+// Exporting Database
+module.exports = database;
---
+++ server.js
@@ -0,0 +1,3 @@
+const sessions = require('express-sessions');
+const express = require('express');
+const app = express();
